72247c2f2b1071fd2b321ef5dca285e9686af4c6
Admin <hoangtrung26d@gmail.com>
2020-07-12 14:33:25 +0700

    BaiTap

---
+++ 7_Pham_Hoang_Trung/Bai1.10.py
@@ -0,0 +1,26 @@
+import math
+#Nhap cac so A, B, C
+a = float(input("Nhap so a ="))
+b = float(input("Nhap so b = "))
+c = float(input("Nhap so c = "))
+
+if(a==0):
+    if(b==0):
+        if (c==0):
+            print("Moi x la nghiem")
+        else:
+            print("Vo nghiem")
+    else:
+        print("Phuong trinh co nghiem don: {} ".format(-c/b))
+
+else:
+    delta =  b**2 - 4*a*c
+    if(delta<0):
+        print("Phuong trinh vo nghiem")
+    else:
+        if(delta==0):
+            print("Phuong trinh nghiem kep: {}".format(-b/(2*a)))
+        else:
+            print("Phuong trinh co 2 nghiem phan biet\n");
+            print("x1 ={}".format((-b - math.sqrt(delta)) / (2 * a)))
+            print("x2 ={}".format((-b + math.sqrt(delta)) / (2 * a)))
---
+++ 7_Pham_Hoang_Trung/Bai1.6.py
@@ -0,0 +1,18 @@
+import random
+score = 0
+somay = random.randint(1,20)
+while(score<5):
+    print('Moi ban doan: ')
+    sonhap= int(input())
+    score +=1
+    if sonhap < somay:
+        print("So ban doan nho hon")
+    elif sonhap> somay:
+        print("So ban nhap lon hon")
+    else:
+        break
+
+if sonhap == somay:
+    print("Ban nhap dung so roi")
+else:
+    print("ban doan sai")
